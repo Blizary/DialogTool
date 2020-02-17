4498a4a114d21f7e8cc338f4bed4dc42f2494fbf
Blizy <rosajunior05@gmail.com>
2020-02-17 09:18:13 +0000

    Begining
    begining

---
+++ DialogTools/Assets/Scripts/Page.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="NewPage",menuName ="Page")]
+public class Page : ScriptableObject
+{
+    public Sprite backgroundImage;
+    [TextArea]
+    public string mainText;
+    public List<PageOption> options;
+}
